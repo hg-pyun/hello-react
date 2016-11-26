70cda85389c30de941ed4225b268e917656bad6d
hg.pyun <hg.pyun@linecorp.com>
2016-11-26 17:15:38 +0900

    state, component mapping

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,15 @@ class App extends React.Component {
                 <JSXExam />
                 <PropsExam name="props example">이사이에 있는거</PropsExam>
                 <PropsExam>이사이에 있는거</PropsExam>
+                <Counter/>
+                <Contact/>
             </div>
         )
     }
 }
-
+/**
+ *  JSX
+ */
 class JSXExam extends React.Component {
     render (){
         let text = "Hello World!";
@@ -28,6 +32,10 @@ class JSXExam extends React.Component {
     }
 }
 
+
+/**
+ *  Props
+ */
 class PropsExam extends React.Component {
     render (){
         return (
@@ -46,5 +54,78 @@ PropsExam.defaultProps = {
 // PropsExam.propTypes = {
 //     name : React.propTypes.string
 // };
+
+/**
+ *  State
+ */
+class Counter extends React.Component {
+
+    constructor(props){
+        super(props); // super props를 해줘야 state에 접근할 수 있음
+        this.state = {
+            value : 0
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        this.setState({
+            value : this.state.value + 1
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>State Example</h1>
+                <h2>{this.state.value}</h2>
+                <button onClick={this.handleClick}>Press Me</button>
+            </div>
+        )
+    }
+}
+
+/**
+ *  Component Mapping
+ */
+class Contact extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            contactData : [
+                {name:'Abet', phone:'010-0000-0001'},
+                {name:'Betty', phone:'010-0000-0002'},
+                {name:'Charlie', phone:'010-0000-0003'},
+                {name:'David', phone:'010-0000-0004'}
+            ]
+        }
+    }
+
+    render(){
+        const mapToComponent = (data) => {
+            return data.map((contact, i) =>{ // callback (currentValue, index, array)
+                console.log(contact, i);
+                return (<ContactInfo contact={contact} key={i}/>); // props로 객체를 넘길수도 있음
+            })
+        };
+
+        return (
+            <div>
+                <h1>Component Mapping</h1>
+                {mapToComponent(this.state.contactData)}
+            </div>
+        )
+    }
+}
+
+class ContactInfo extends React.Component {
+    render(){
+        return (
+            <div>{this.props.contact.name} {this.props.contact.phone}</div>
+        )
+    }
+}
 
 export default App;
