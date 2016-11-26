c5a0b6397c0152f365e01ad5f15f5cca47c6126b
hg.pyun <hg.pyun@linecorp.com>
2016-11-26 16:37:51 +0900

    props

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 class App extends React.Component {
-    render(){
+    render() {
+        return (
+            <div>
+                <JSXExam />
+                <PropsExam name="props example">이사이에 있는거</PropsExam>
+                <PropsExam>이사이에 있는거</PropsExam>
+            </div>
+        )
+    }
+}
+
+class JSXExam extends React.Component {
+    render (){
         let text = "Hello World!";
         let style = {
             backgroundColor : 'aqua'
@@ -9,11 +21,30 @@ class App extends React.Component {
         return (
             <div>
                 /* Comment */
-                <h1 style={style}>{text}</h1>
-                <p>this is p</p>
+                <h1> JSX Example</h1>
+                <p style={style}>{text}</p>
             </div>
         );
     }
 }
+
+class PropsExam extends React.Component {
+    render (){
+        return (
+            <div>
+                <h1>{this.props.name}</h1>
+                <p>{this.props.children}</p>
+            </div>
+        )
+    }
+}
+
+PropsExam.defaultProps = {
+    name : "Props Unknown"
+};
+
+// PropsExam.propTypes = {
+//     name : React.propTypes.string
+// };
 
 export default App;
